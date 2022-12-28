0e0f9fff9504a48492fbab3dfc9df0423dd2589d
Ali shooshtari <shooshtariali05@gmail.com>
2022-12-28 19:54:33 +0330

    initial comment

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/HeaderComponents/Header";
+import Main from "./components/MainComponents/Main";
+import Footer from "./components/FooterComponents/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
